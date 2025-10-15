546c828730859afb3e0d6fd6acd845805584a35b
MatiasMtz <matimartinez1000@gmail.com>
2025-10-15 18:51:58 -0300

    Cambios: 1. Clase Jugador creada. attr y metodos de acceso

---
+++ tateti-obl-p2/src/tateti/obl/p2/Jugador.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tateti.obl.p2;
+
+/**
+ *
+ * @author matim
+ */
+public class Jugador {
+    private String nombre;
+    private int edad;
+    private int partidasJugadas;
+    private int partidasGanadas;
+    private int partidasEmpatadas;
+    
+    public Jugador(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.partidasJugadas = 0;
+        this.partidasGanadas = 0;
+        this.partidasEmpatadas = 0;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getEdad() {
+        return this.edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void registrarPartida(String resultado) {
+        this.partidasJugadas++;
+        if (resultado.equals("victoria")) {
+            this.partidasGanadas++;
+        } else if (resultado.equals("empate")) {
+            this.partidasEmpatadas++;
+        }
+    }
+    
+    public int getPartidasPerdidas() {
+        return this.partidasJugadas - (this.partidasGanadas + this.partidasEmpatadas);
+    }
+}
