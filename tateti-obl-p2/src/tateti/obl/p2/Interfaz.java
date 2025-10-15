be4fb99ff783e681ee3a56d230a5277c17bd0c34
MatiasMtz <matimartinez1000@gmail.com>
2025-10-15 18:49:40 -0300

    Cambios: 1. registrarJugador creado. Muestra menu, pide input del usuario e intenta crear Jugador a traves de la clase Sistema

--- tateti-obl-p2/src/tateti/obl/p2/Interfaz.java
+++ tateti-obl-p2/src/tateti/obl/p2/Interfaz.java
@@ -11,11 +11,17 @@ import java.util.*;
  */
 public class Interfaz {
     // private Sistema sistema = new Sistema();
+    private Sistema sistema;
     Scanner scanner = new Scanner(System.in);
+    
+    public Interfaz(Sistema sistema) {
+        this.sistema = sistema;
+    }
     
     public boolean iniciar() {
         menuPrincipal();  
         int seleccion = leerInput(5);
+        scanner.nextLine(); // limpiar buffer de scanner porque mantiene \n de la lectura.
         
         switch (seleccion) {
             case 1:
@@ -58,11 +64,47 @@ public class Interfaz {
     }
     
     public void registrarJugador() {
-        System.out.println("MENU PARA REGISTRAR JUGADORES");
+        System.out.println("*********************************");
+        System.out.println("*****-=[REGISTRAR JUGADOR]=-*****");
+        System.out.println("*********************************");
+        boolean creado = false;
+        
+        while (!creado) {
+            System.out.println("Ingrese el nombre del jugador (o presione ENTER para volver al menú principal): ");
+            String nombre = scanner.nextLine().trim();
+            
+            if (nombre.isEmpty()) {
+                System.out.println("Volviendo al menú principal...");
+                return;
+            }
+            
+            System.out.println("Ingrese la edad del jugador: ");
+            int edad;
+            try {
+                edad = Integer.parseInt(scanner.nextLine());
+                if (edad <= 0) {
+                    System.out.println("La edad debe ser un número positivo.");
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Ingrese un número válido para la edad.");
+                continue;
+            }
+            
+            creado = sistema.crearJugador(nombre, edad);
+            if (creado) {
+                System.out.println("El jugador " + nombre + " ha sido creado correctamente.");
+                System.out.println("Presione ENTER para volver al menú principal...");
+                scanner.nextLine(); // Esperar la confirmacion del usuario para volver al menu principal.
+            } else {
+                System.out.println("El jugador " + nombre + " ya existe. Intente con otro nombre.");
+            }
+        }
     }
     
     public void comenzarPartidaComun() {
         System.out.println("COMENZAR PARTIDA...");
+        // mostrar lista numerada de jugadores
     }
     
     public void continuarPartida() {
